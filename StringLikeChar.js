67e949016dd4c8231649b8b64831be0574b9a4ba
adbarc92 <adbarclay92@gmail.com>
2020-10-08 15:30:11 -0600

    adds reduce attempt

--- StringLikeChar.js
+++ StringLikeChar.js
@@ -9,11 +9,6 @@ String.prototype.map = function (fn) {
 
 const testMap = (elem) => elem + 'a';
 
-// String.prototype.filter = function (fn) {
-//   const strArr = this.split('');
-//   return Array.prototype.filter.apply(strArr, fn);
-// };
-
 String.prototype.filter = function (fn) {
   const strArr = this.split('');
   return strArr.filter((elem) => fn(elem)).join('');
@@ -24,11 +19,27 @@ const testFilter = (elem) => elem === 'a';
 String.prototype.join = function (symb) {
   const strArr = this.split('');
   const strArr2 = [];
-  if (typeof symb !== String) {
-    const symb2 = symb.toString();
-  } else symb2 = symb;
   for (i in strArr) {
-    strArr2.push(i + symb);
+    strArr2.push(strArr[i] + symb);
   }
   return strArr2.join('');
 };
+
+String.prototype.reduce = function (fn) {
+  const strArr = this.split('');
+  let ret = 0;
+  for (i in strArr) {
+    ret = fn(ret, strArr[i]);
+  }
+  return ret;
+};
+
+const testReduce1 = (a, b) => {
+  return a + b;
+};
+
+const testReduce2 = (a, b) => {
+  return a * b;
+};
+
+const str2 = 'pizza';
